89791438145b79e6ec5958992fbff6edd716699c
Christian Aranas <aranaschristianlouise@gmail.com>
2023-11-28 16:24:12 +0800

    Changed file image pointers in columnContent

--- assets/js/test.js
+++ assets/js/test.js
@@ -2,129 +2,129 @@ const columnContent = [
   {
     question1: "I like to work on cars",
     question2: "I like to build things",
-    imageSrc1: "test/1.png",
-    imageSrc2: "test/2.png"
+    imageSrc1: "assets/img/test/1.png",
+    imageSrc2: "assets/img/test/2.png"
   },
   {
     question1: "I like to take care of animals",
     question2: "I like putting things together <br>or assembling things",
-    imageSrc1: "test/3.png",
-    imageSrc2: "test/4.png"
+    imageSrc1: "assets/img/test/3.png",
+    imageSrc2: "assets/img/test/4.png"
   },
   {
     question1: "I like to cook",
     question2: "I am a practical person",
-    imageSrc1: "test/5.png",
-    imageSrc2: "test/6.png"
+    imageSrc1: "assets/img/test/5.png",
+    imageSrc2: "assets/img/test/6.png"
   },
   {
     question1: "I like working outdoors",
     question2: "I like to do puzzles",
-    imageSrc1: "test/7.png",
-    imageSrc2: "test/8.png"
+    imageSrc1: "assets/img/test/7.png",
+    imageSrc2: "assets/img/test/8.png"
   },
   // 
   {
     question1: "I like to do experiments",
     question2: "I enjoy science",
-    imageSrc1: "test/9.png",
-    imageSrc2: "test/nx10.png"
+    imageSrc1: "assets/img/test/9.png",
+    imageSrc2: "assets/img/test/nx10.png"
   },
   {
     question1: "I enjoy trying to figure out how things work",
     question2: "I like to analyze things (problems/situations)",
-    imageSrc1: "test/nx11.png",
-    imageSrc2: "test/nx12.png"
+    imageSrc1: "assets/img/test/nx11.png",
+    imageSrc2: "assets/img/test/nx12.png"
   },
   {
     question1: "I like working with numbers or charts",
     question2: "I’m good at math",
-    imageSrc1: "test/nx13.png",
-    imageSrc2: "test/nx14.png"
+    imageSrc1: "assets/img/test/nx13.png",
+    imageSrc2: "assets/img/test/nx14.png"
   },
   {
     question1: "I am good at working independently",
     question2: "I like to read about art and music",
-    imageSrc1: "test/nx15.png",
-    imageSrc2: "test/nx16.png"
+    imageSrc1: "assets/img/test/nx15.png",
+    imageSrc2: "assets/img/test/nx16.png"
   },
   {
     question1: "I enjoy creative writing",
     question2: "I am a creative person",
-    imageSrc1: "test/nx17.png",
-    imageSrc2: "test/nx18.png"
+    imageSrc1: "assets/img/test/nx17.png",
+    imageSrc2: "assets/img/test/nx18.png"
   },
   {
     question1: "I like to play instruments or sing",
     question2: "I like acting in plays",
-    imageSrc1: "test/nx19.png",
-    imageSrc2: "test/nx21.png"
+    imageSrc1: "assets/img/test/nx19.png",
+    imageSrc2: "assets/img/test/nx21.png"
   },
   {
     question1: "I like to draw",
     question2: "I like to work in teams.",
-    imageSrc1: "test/nx22.png",
-    imageSrc2: "test/nx23.png"
+    imageSrc1: "assets/img/test/nx22.png",
+    imageSrc2: "assets/img/test/nx23.png"
   },
   {
     question1: "I like to teach or train people",
     question2: "I like trying to help people solve their problems",
-    imageSrc1: "test/nx24.png",
-    imageSrc2: "test/nx25.png"
+    imageSrc1: "assets/img/test/nx24.png",
+    imageSrc2: "assets/img/test/nx25.png"
   },
   {
     question1: "I am interested in healing people",
     question2: "I enjoy learning about other cultures",
-    imageSrc1: "test/nx26.png",
-    imageSrc2: "test/nx27.png"
+    imageSrc1: "assets/img/test/nx26.png",
+    imageSrc2: "assets/img/test/nx27.png"
   },
   {
     question1: "I like to get into discussions about issues",
     question2: "I liked helping people ",
-    imageSrc1: "test/nx28.png",
-    imageSrc2: "test/nx29.png"
+    imageSrc1: "assets/img/test/nx28.png",
+    imageSrc2: "assets/img/test/nx29.png"
   },
   {
     question1: "I am an ambitious person,I set goals for myself",
     question2: "I like to influence or persuade people",
-    imageSrc1: "test/nx30.png",
-    imageSrc2: "test/nx31.png"
+    imageSrc1: "assets/img/test/nx30.png",
+    imageSrc2: "assets/img/test/nx31.png"
   },
   {
     question1: "I like selling things",
     question2: "I am quick to take on new responsibilities",
-    imageSrc1: "test/nx32.png",
-    imageSrc2: "test/nx33.png"
+    imageSrc1: "assets/img/test/nx32.png",
+    imageSrc2: "assets/img/test/nx33.png"
   },
   {
     question1: "I would like yo start my own business",
     question2: "I like to lead",
-    imageSrc1: "test/nx34.png",
-    imageSrc2: "test/nx35.png"
+    imageSrc1: "assets/img/test/nx34.png",
+    imageSrc2: "assets/img/test/nx35.png"
   },
   {
     question1: "I like to give speeches",
     question2: "I like to organize things",
-    imageSrc1: "test/nx36.png",
-    imageSrc2: "test/nx37.png"
+    imageSrc1: "assets/img/test/nx36.png",
+    imageSrc2: "assets/img/test/nx37.png"
   },
   {
     question1: "I like to have clear instructions to follow",
     question2: "I wouldnt mind working 8 hours per day in an office",
-    imageSrc1: "test/nx38.png",
-    imageSrc2: "test/nx39.png"
+    imageSrc1: "assets/img/test/nx38.png",
+    imageSrc2: "assets/img/test/nx39.png"
   },
   {
     question1: "I pay attention to details",
     question2: "I like todo filing or typing",
-    imageSrc1: "test/nx40.png",
-    imageSrc2: "test/nx41.png"
+    imageSrc1: "assets/img/test/nx40.png",
+    imageSrc2: "assets/img/test/nx41.png"
   },
   {
     question1: "I am good at keeping records of my work",
     question2: "I would like to work in an office",
-    imageSrc1: "test/nx42.png",
-    imageSrc2: "test/nx43.png"
+    imageSrc1: "assets/img/test/nx42.png",
+    imageSrc2: "assets/img/test/nx43.png"
   },
 
 ];
